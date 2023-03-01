2631439674adf1b6445a6d5f0cc9c67bc959ce6e
roshith p <roshith844@gmail.com>
2023-03-01 17:25:49 +0530

    chore: change latest post number to 20

--- src/app/use-cases/get-data-from-database/get-blog.js
+++ src/app/use-cases/get-data-from-database/get-blog.js
@@ -1,10 +1,11 @@
 const blogContentService = require('./../../data-access/blog-content-service')
 
 async function getBlogCards() {
-    let BLOG_CONTENT = await blogContentService.getLatestBlogs(2)
+    let BLOG_CONTENT = await blogContentService.getLatestBlogs(20)
 
     // Takes only first 200 words from content
     for (let i = 0; i < BLOG_CONTENT.length; i++) {
+    
         BLOG_CONTENT[i].content = BLOG_CONTENT[i].content.substr(0, 200)
     }
 
