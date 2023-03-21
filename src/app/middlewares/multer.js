6c9e12949751c9cd6b2e97b49022e562cc2b952d
ROSHITH P <roshith844@gmail.com>
2023-03-21 17:45:38 +0530

    chore: remove unwanted comments

--- src/app/middlewares/multer.js
+++ src/app/middlewares/multer.js
@@ -11,37 +11,5 @@ const storage = multer.diskStorage({
     }
 })
 
-// upload.single("image")
-// puton data enctype="multipart/form-data"   
-// tyoe file name image
 const uploadProfilePicture = multer({ storage: storage })
 module.exports = uploadProfilePicture
-
-// const multer = require('multer')
-// const path = require('path')
-
-// const storage = multer.diskStorage({
-//      destination: (req, file,callback)=>{
-//           callback(null, './public/images/')
-//      },
-//      filename: (req, file, callback)=>{
-//           callback(null, new Date().toISOString() +'-' +  file.originalname )
-//      }
-// })
-
-// // File Validation
-// const fileFilter = (req, file, callback)=>{
-//     if(file.mimetype == 'image/jpeg' || file.mimetype == file.mimetype == 'image/png'  ){
-//         callback(null, true)
-//     }else{
-//         // Prevents Upload
-//         callback({message: 'Unsupported File Format'}, false)
-
-//     }
-// }
-
-// // upload.single("image")
-// // puton data enctype="multipart/form-data"
-// // tyoe file name image
-// const upload = multer({storage: storage, limits: {fileSize: 1024* 1024}, fileFilter: fileFilter })
-// module.exports= upload
