52e8e32b224f68194f4082084f469a4288b369a6
Roshith P <roshith844@gmail.com>
2024-10-24 12:54:18 +0530

    fix: profile page

--- src/app/controllers/profileController.js
+++ src/app/controllers/profileController.js
@@ -1,60 +1,94 @@
-const cloudinary = require("../config/cloudinary")
-const { getUserRole, getUserDetails } = require("../use-cases/get-data-from-database/get-user-details")
-const { editProfilebyId } = require("../use-cases/save-to-database/update-user-data")
-const tokenManagement = require("../use-cases/token/jwt-token-management")
-const { saveUserProfilePictureUrl } = require("../use-cases/upload-profile-picture/save-image-url")
+const cloudinary = require("../config/cloudinary");
+const {
+  getUserRole,
+  getUserDetails,
+} = require("../use-cases/get-data-from-database/get-user-details");
+const {
+  editProfilebyId,
+} = require("../use-cases/save-to-database/update-user-data");
+const tokenManagement = require("../use-cases/token/jwt-token-management");
+const {
+  saveUserProfilePictureUrl,
+} = require("../use-cases/upload-profile-picture/save-image-url");
+const jwtTokenManagement = require("../use-cases/token/jwt-token-management");
+const userDetailsManagement = require("../use-cases/get-data-from-database/get-user-details");
+
 // decodeJwtToken
 module.exports = {
-    getProfile: async (req, res, next) => {
-        const USER_ID = req.user
-        const USER_DETAILS = await getUserDetails(USER_ID)
-        if (USER_DETAILS === false || USER_DETAILS === null || USER_DETAILS.length === 0) {
-            res.json({
-                "success": false
-            })
-            res.end()
-            return
-        } else {
-            res.json({
-                "success": true,
-                "name": USER_DETAILS.name,
-                "email": USER_DETAILS.email,
-                "phone": USER_DETAILS.phone,
-                "image": USER_DETAILS.profie_picture_url,
-                "role": USER_DETAILS.role
-            })
-        }
-    },
-
-    editProfile: async (req, res, next) => {
-        const USER_ID = req.user
-        const { name, email, phone } = req.body
-        if (await editProfilebyId(USER_ID, name, email, phone) === true) {
-            res.json({ "success": true })
-        } else {
-            res.json({ "success": false })
-        }
-    },
-
-    editProfileImage: async (req, res, next) => {
-
-        // Uploads to cloudinary
-        const RESULT = await cloudinary.uploader.upload(req.file.path, {
-            public_id: `${req.user}_profile`,
-            width: 500,
-            height: 500,
-            crop: 'fill'
-        })
-
-        const USER_ID = req.user
-        if (!RESULT) return res.json(res.json({ "success": false, "message": "not uploaded to cloudinary" }))
-
-        const IMAGE_URL = RESULT.url
-
-        if (await saveUserProfilePictureUrl(USER_ID, IMAGE_URL) === true) {
-            res.json({ "success": true, "data": IMAGE_URL })
-        } else {
-            res.json({ "success": false })
-        }
+  getProfile: async (req, res, next) => {
+    // const USER_ID = req.user
+    const USER_EMAIL = jwtTokenManagement.getUserEmailFromToken(
+      req.headers.authorization
+    );
+    if (USER_EMAIL == false) {
+      res.status(403).json({
+        success: false,
+      });
+      return;
     }
-}
+    const USER_ID = await userDetailsManagement.getDocumentId(USER_EMAIL);
+    if (USER_ID == false) {
+      res.status(403).json({
+        success: false,
+      });
+      return;
+    }
+
+    const USER_DETAILS = await getUserDetails(USER_ID);
+    console.log(USER_ID, USER_DETAILS);
+    if (
+      USER_DETAILS === false ||
+      USER_DETAILS === null ||
+      USER_DETAILS.length === 0
+    ) {
+      res.json({
+        success: false,
+      });
+      res.end();
+      return;
+    } else {
+      res.json({
+        success: true,
+        name: USER_DETAILS.name,
+        email: USER_DETAILS.email,
+        phone: USER_DETAILS.phone,
+        image: USER_DETAILS.profie_picture_url,
+        role: USER_DETAILS.role,
+      });
+    }
+  },
+
+  editProfile: async (req, res, next) => {
+    const USER_ID = req.user;
+    const { name, email, phone } = req.body;
+    if ((await editProfilebyId(USER_ID, name, email, phone)) === true) {
+      res.json({ success: true });
+    } else {
+      res.json({ success: false });
+    }
+  },
+
+  editProfileImage: async (req, res, next) => {
+    // Uploads to cloudinary
+    const RESULT = await cloudinary.uploader.upload(req.file.path, {
+      public_id: `${req.user}_profile`,
+      width: 500,
+      height: 500,
+      crop: "fill",
+    });
+
+    const USER_ID = req.user;
+    if (!RESULT)
+      return res.json(
+        res.json({ success: false, message: "not uploaded to cloudinary" })
+      );
+
+    const IMAGE_URL = RESULT.url;
+
+    if ((await saveUserProfilePictureUrl(USER_ID, IMAGE_URL)) === true) {
+      res.json({ success: true, data: IMAGE_URL });
+    } else {
+      res.json({ success: false });
+    }
+  },
+};
--- src/app/routes/user/interactions.js
+++ src/app/routes/user/interactions.js
@@ -4,6 +4,7 @@ const router = express.Router()
 const interactionsController = require('./../../controllers/user/interactionsController')
 const verifyTokenMiddleware = require('./../../middlewares/user/verifyToken')
 
+
 router.patch('/favorites/add-or-remove', verifyTokenMiddleware.verifyUser, tryCatch(interactionsController.addOrRemoveFromFavorites))
 router.get('/favorites/get/cards', verifyTokenMiddleware.verifyUser, tryCatch(interactionsController.getFavoritesCards))
 
