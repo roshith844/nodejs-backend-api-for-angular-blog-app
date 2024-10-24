04a53687a6f6889caa369a3236ce29231ba7bf10
Roshith P <roshith844@gmail.com>
2024-10-24 16:03:43 +0530

    fix: apply for writer

--- src/app/controllers/user/become-writer-controller.js
+++ src/app/controllers/user/become-writer-controller.js
@@ -1,18 +1,37 @@
-const { updateRoleToWriter } = require("../../use-cases/save-to-database/update-user-data")
+const {
+  updateRoleToWriter,
+} = require("../../use-cases/save-to-database/update-user-data");
+const jwtTokenManagement = require('../../use-cases/token/jwt-token-management')
+const userDetailsManagement = require('./../../use-cases/get-data-from-database/get-user-details')
 
 module.exports = {
-    applyForWriterRole: async (req, res, next) => {
-        const USER_ID = req.user
-        const IS_UPDATED = await updateRoleToWriter(USER_ID)
-        if (IS_UPDATED) {
-            res.json({
-                "success": true,
-                "isWriter": true
-            })
-        } else {
-            res.json({ "success": false, "isWriter": false })
-
-        }
-
+  applyForWriterRole: async (req, res, next) => {
+    const USER_EMAIL = jwtTokenManagement.getUserEmailFromToken(
+      req.headers.authorization
+    );
+    if (USER_EMAIL == false) {
+      res.status(403).json({
+        success: false,
+      });
+      return;
     }
-}
+    // Take userId from database
+    const USER_ID = await userDetailsManagement.getDocumentId(USER_EMAIL);
+    if (USER_ID == false) {
+      res.status(403).json({
+        success: false,
+      });
+      return;
+    }
+
+    const IS_UPDATED = await updateRoleToWriter(USER_ID);
+    if (IS_UPDATED) {
+      res.json({
+        success: true,
+        isWriter: true,
+      });
+    } else {
+      res.json({ success: false, isWriter: false });
+    }
+  },
+};
